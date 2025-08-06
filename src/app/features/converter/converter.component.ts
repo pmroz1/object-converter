62995fc3919c6635c03af1022b442651e2a021d9
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-08-06 14:03:24 +0200

    feat: enhance converter UI with structured layout and improved labels

--- src/app/features/converter/converter.component.ts
+++ src/app/features/converter/converter.component.ts
@@ -4,7 +4,13 @@ import { TextInputOutputAreaComponent } from '../../shared/text-input-output-are
 @Component({
   selector: 'app-converter',
   imports: [TextInputOutputAreaComponent],
-  template: ` <app-text-input-output-area></app-text-input-output-area>`,
+  template: `
+    <div class="card flex flex-col items-center justify-center h-full w-full m-10">
+      <h1 class="text-7xl font-bold mb-4">JSON Object Converter</h1>
+      <p class="mb-4 text-3xl">Convert your objects easily!</p>  
+      <app-text-input-output-area></app-text-input-output-area>
+    </div>
+  `,
   styles: '',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- src/app/shared/text-input-output-area/text-input-output-area.component.ts
+++ src/app/shared/text-input-output-area/text-input-output-area.component.ts
@@ -7,29 +7,31 @@ import { FloatLabel } from 'primeng/floatlabel';
   selector: 'app-text-input-output-area',
   imports: [FormsModule, TextareaModule, FloatLabel],
   template: `
-    <p-floatlabel>
-      <textarea
-        pTextarea
-        id="over_label"
-        rows="5"
-        cols="30"
-        style="resize: none"
-        class="h-full"
-      ></textarea>
-      <label for="over_label">Over Label</label>
-    </p-floatlabel>
+    <div class="card flex flex-wrap justify-center items-stretch gap-4">
+      <p-floatlabel>
+        <textarea
+          pTextarea
+          id="over_label2"
+          rows="5"
+          cols="30"
+          style="resize: none"
+          class="h-full"
+        ></textarea>
+        <label for="JSON">JSON</label>
+      </p-floatlabel>
 
-    <p-floatlabel variant="in">
-      <textarea
-        pTextarea
-        id="over_label"
-        rows="5"
-        cols="30"
-        style="resize: none"
-        class="h-full"
-      ></textarea>
-      <label for="in_label">In Label</label>
-    </p-floatlabel>
+      <p-floatlabel>
+        <textarea
+          pTextarea
+          id="over_label"
+          rows="5"
+          cols="30"
+          style="resize: none"
+          class="h-full"
+        ></textarea>
+        <label for="Output">Output</label>
+      </p-floatlabel>
+    </div>
   `,
   styles: '',
   changeDetection: ChangeDetectionStrategy.OnPush,
